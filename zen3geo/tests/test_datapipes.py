64a1565b618aca219e570de3e15a54f0e09d17e8
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-06-06 16:13:53 -0400

    :white_check_mark: Check that RioXarrayReader's len works
    For 100% code coverage. Also expanded the test funtion's docstring.

--- zen3geo/tests/test_datapipes.py
+++ zen3geo/tests/test_datapipes.py
@@ -9,7 +9,8 @@ from zen3geo import RioXarrayReader
 # %%
 def test_rioxarray_reader():
     """
-    Ensure that RioXarrayReader works to read in a GeoTIFF file.
+    Ensure that RioXarrayReader works to read in a GeoTIFF file and outputs a
+    tuple made up of a filename and an xarray.DataArray object.
     """
     file_url: str = "https://github.com/GenericMappingTools/gmtserver-admin/raw/master/cache/earth_day_HD.tif"
     dp = IterableWrapper(iterable=[file_url])
@@ -19,6 +20,7 @@ def test_rioxarray_reader():
     # Using functional form (recommended)
     dp_rioxarray = dp.read_from_rioxarray()
 
+    assert len(dp_rioxarray) == 1
     it = iter(dp_rioxarray)
     filename, dataarray = next(it)
 
