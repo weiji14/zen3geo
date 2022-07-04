f138c10415688a8b54c01d10a61b69b9fcf614c6
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-07-04 16:12:55 -0700

    :sparkles: XbatcherSlicerIterDataPipe for slicing xarray.DataArray
    An interable-style DataPipe for creating chips from xarray.DataArray objects! Uses xbatcher to do the windowed slicing. Included a doctest and a unit test, added a new section in the API docs and an intersphinx mapping for xbatcher.

--- zen3geo/datapipes/__init__.py
+++ zen3geo/datapipes/__init__.py
@@ -4,3 +4,4 @@ Iterable-style DataPipes for geospatial raster 🌈 and vector 🚏 data.
 
 from zen3geo.datapipes.pyogrio import PyogrioReaderIterDataPipe as PyogrioReader
 from zen3geo.datapipes.rioxarray import RioXarrayReaderIterDataPipe as RioXarrayReader
+from zen3geo.datapipes.xbatcher import XbatcherSlicerIterDataPipe as XbatcherSlicer
