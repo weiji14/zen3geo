3f8d0cb765f4cfd3af7021d8336c52ac4f6fe426
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-07-05 20:51:58 -0700

    :recycle: Let RioXarrayReader return dataarray only instead of tuple (#24)
    So the source filepath/url in an xarray.DataArray is actually in `dataarray.encoding["source"]` already, no need to return a tuple of (filepath, dataarray). This helps to streamline downstream slicing tasks by having only one object to deal with.

--- zen3geo/tests/test_datapipes_rioxarray.py
+++ zen3geo/tests/test_datapipes_rioxarray.py
@@ -9,8 +9,8 @@ from zen3geo.datapipes import RioXarrayReader
 # %%
 def test_rioxarray_reader():
     """
-    Ensure that RioXarrayReader works to read in a GeoTIFF file and outputs a
-    tuple made up of a filename and an xarray.DataArray object.
+    Ensure that RioXarrayReader works to read in a GeoTIFF file and outputs an
+    xarray.DataArray object.
     """
     file_url: str = "https://github.com/GenericMappingTools/gmtserver-admin/raw/master/cache/earth_day_HD.tif"
     dp = IterableWrapper(iterable=[file_url])
@@ -22,8 +22,7 @@ def test_rioxarray_reader():
 
     assert len(dp_rioxarray) == 1
     it = iter(dp_rioxarray)
-    filename, dataarray = next(it)
+    dataarray = next(it)
 
-    assert isinstance(filename, str)
     assert dataarray.shape == (1, 960, 1920)
     assert dataarray.dims == ("band", "y", "x")
