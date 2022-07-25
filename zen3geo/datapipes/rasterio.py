0a4e15b7cc548cb76ed39e504e3346b117f3bec2
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-07-24 23:05:34 -0400

    :poop: Allow using template xarray.DataArray to rasterize vector shapes
    Convenient but complicated way of rasterizing vector data into a template xarray.DataArray. The vector and raster IterDataPipe objects are Zipped, with the vector IterDataPipe being broadcast/repeated to match the length of the raster IterDataPipe. Affine transformation is obtained directly from xarray.Datarray via the `.rio.transform()` accessor property whenever possible. Automatic reprojection to the xarray.DataArray's coordinate reference system is done if needed (and assuming both have a `crs` attribute). The unit test feels like the length of an integration test, but it captures the key functionalities needed and uses a real-ish dataset.

--- zen3geo/datapipes/rasterio.py
+++ zen3geo/datapipes/rasterio.py
@@ -1,33 +1,56 @@
 """
 DataPipes for :doc:`rasterio <rasterio:index>`.
 """
-from typing import Any, Dict, Iterator, Optional, Union
+from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
 
+import affine
 import numpy as np
 import rasterio
+import xarray as xr
 from torchdata.datapipes import functional_datapipe
-from torchdata.datapipes.iter import IterDataPipe
-from torchdata.datapipes.utils import StreamWrapper
+from torchdata.datapipes.iter import IterableWrapper, IterDataPipe
 
 
 @functional_datapipe("rasterize_with_rasterio")
 class RasterioRasterizerIterDataPipe(IterDataPipe):
     """
-    Takes a vector :py:class:`shapely.geometry.GeometryCollection`,
-    :py:class:`geopandas.GeoSeries` or :py:class:`geopandas.GeoDataFrame` and
-    yields a raster :py:class:`numpy.ndarray` or :py:class:`xarray.DataArray`
-    image with input geometries burned in
-    (functional name: ``rasterize_with_rasterio``).
+    Takes a vector :py:class:`geopandas.GeoSeries`,
+    :py:class:`geopandas.GeoDataFrame` or
+    :py:class:`shapely.geometry.GeometryCollection`, and yields a raster
+    :py:class:`xarray.DataArray` or :py:class:`numpy.ndarray` image with input
+    geometries burned in (functional name: ``rasterize_with_rasterio``).
 
     Parameters
     ----------
     source_datapipe : IterDataPipe[geopandas.GeoDataFrame]
         A DataPipe that contains geometries or (`geometry`, `value`) pairs. The
-        `geometry` can either be an object that implements the geo interface
-        (e.g. :py:class:`shapely.geometry.GeometryCollection`, :py:class:`geopandas.GeoSeries` or
-        :py:class:`geopandas.GeoDataFrame`) or a GeoJSON-like object. If no
-        `value` is provided the `default_value` will be used. If `value` is
-        `None` the `fill` value will be used.
+        `geometry` can either be:
+
+        - :py:class:`geopandas.GeoSeries` or :py:class:`geopandas.GeoDataFrame`
+          (recommended)
+        - :py:class:`shapely.geometry.GeometryCollection` or other
+          ``shapely.geometry`` object (Point, Line, Polygon)
+        - a GeoJSON-like :py:class:`dict` object
+        - an object that implements the geo interface (see
+          https://gist.github.com/sgillies/2217756#__geo_interface__)
+
+        If no `value` is provided the `default_value` will be used. If `value`
+        is `None` the `fill` value will be used.
+
+    out : IterDataPipe[xarray.DataArray]
+        Optional. An :py:class:`xarray.DataArray` (recommended) or
+        :py:class:`numpy.ndarray` with a given shape in which to store results.
+
+    out_shape : tuple[int, int] or list[int, int]
+        Required if ``out`` is not set. Shape of output numpy ndarray.
+
+    transform : affine.Affine
+        Optional. An affine.Affine object (e.g. ``from affine import Affine;
+        Affine(30.0, 0.0, 548040.0, 0.0, -30.0, "6886890.0)`` giving the affine
+        transformation used to convert raster coordinates (e.g. [0, 0]) to
+        geographic coordinates. If none is provided, the function will attempt
+        to obtain an affine transformation from the xarray object (i.e. using
+        ``out.rio.transform()``).
 
     kwargs : Optional
         Extra keyword arguments to pass to
@@ -35,9 +58,14 @@ class RasterioRasterizerIterDataPipe(IterDataPipe):
 
     Yields
     ------
-    image : numpy.ndarray or xarray.DataArray
-        An :py:class:`numpy.ndarray` or :py:class:`xarray.DataArray` object
-        containing the raster data.
+    image : xarray.DataArray or numpy.ndarray
+        An array-like object containing the raster data. The return type
+        depends on the ``out`` or ``out_shape`` parameter:
+
+        - :py:class:`xarray.DataArray` if ``out`` is an
+          :py:class:`xarray.DataArray`
+        - :py:class:`numpy.ndarray` if ``out`` is a :py:class:`numpy.ndarray`
+          or if ``out_shape`` is set.
 
     Example
     -------
@@ -63,14 +91,54 @@ class RasterioRasterizerIterDataPipe(IterDataPipe):
     """
 
     def __init__(
-        self, source_datapipe: IterDataPipe, **kwargs: Optional[Dict[str, Any]]
+        self,
+        source_datapipe: IterDataPipe,
+        out: Optional[
+            Union[IterDataPipe[xr.DataArray], IterDataPipe[np.ndarray]]
+        ] = IterableWrapper([None]),
+        out_shape: Optional[
+            Union[IterDataPipe[Tuple[int]], IterDataPipe[List[int]]]
+        ] = None,
+        transform: rasterio.Affine = rasterio.transform.IDENTITY,
+        **kwargs: Optional[Dict[str, Any]]
     ) -> None:
         self.source_datapipe: IterDataPipe = source_datapipe
+        self.out: IterDataPipe = out
+        self.out_shape: IterDataPipe = out_shape
+        self.transform: rasterio.Affine = transform
         self.kwargs = kwargs
 
-    def __iter__(self) -> Iterator[Union[np.ndarray]]:
-        for geodataframe in self.source_datapipe:
-            yield rasterio.features.rasterize(shapes=geodataframe, **self.kwargs)
+        self.len_iter_vector = len(self.source_datapipe)
+        self.len_iter_raster = len(self.out)
+
+    def __iter__(self) -> Iterator[Union[xr.DataArray, np.ndarray]]:
+        # Broadcast vector iterator to match length of raster iterator
+        fill_value = (
+            list(self.source_datapipe)[0] if self.len_iter_vector == 1 else None
+        )
+        for _out, _geoseries in self.out.zip_longest(
+            self.source_datapipe, fill_value=fill_value
+        ):
+            if hasattr(_out, "rio"):  # if raster has rioxarray accessors
+                # Get affine transform from template raster
+                _out = _out * 0  # empty xarray.DataArray template
+                if self.transform is rasterio.transform.IDENTITY:
+                    self.transform = _out.rio.transform()
+
+                # Reproject vector to same coordinate reference system as the
+                # template raster image
+                if hasattr(_geoseries, "crs") and _geoseries.crs != _out.rio.crs:
+                    _geoseries: gpd.GeoSeries = _geoseries.to_crs(
+                        crs=_out.rio.crs
+                    ).geometry
+
+            yield rasterio.features.rasterize(
+                shapes=_geoseries,
+                out=_out,
+                out_shape=self.out_shape,
+                transform=self.transform,
+                **self.kwargs
+            )
 
     def __len__(self) -> int:
-        return len(self.source_datapipe)
+        return max(self.len_iter_vector, self.len_iter_raster)
--- zen3geo/tests/test_datapipes_rasterio.py
+++ zen3geo/tests/test_datapipes_rasterio.py
@@ -3,13 +3,15 @@ Tests for rasterio datapipes.
 """
 import numpy as np
 import pytest
+import rioxarray
+import xarray as xr
 from torchdata.datapipes.iter import IterableWrapper
 
 from zen3geo.datapipes import RasterioRasterizer
 
 
 # %%
-def test_rasterio_rasterizer_geoseries():
+def test_rasterio_rasterizer_geoseries_to_ndarray():
     """
     Ensure that RasterioRasterizer works to rasterize a geopandas.GeoSeries
     object into a numpy.ndarray image.
@@ -48,3 +50,55 @@ def test_rasterio_rasterizer_geoseries():
             dtype=np.uint8,
         ),
     )
+
+
+def test_rasterio_rasterizer_geodataframe_to_dataarray():
+    """
+    Ensure that RasterioRasterizer works to rasterize a geopandas.GeoDataFrame
+    object into an xarray.DataArray image.
+    """
+    gpd = pytest.importorskip("geopandas")
+    shapely = pytest.importorskip("shapely")
+
+    # Vector data
+    geodataframe = gpd.GeoDataFrame(
+        data={
+            "geometry": [
+                shapely.geometry.box(minx=-20, miny=30, maxx=0, maxy=60),
+                shapely.geometry.box(minx=0, miny=0, maxx=20, maxy=30),
+                shapely.geometry.box(minx=20, miny=-30, maxx=40, maxy=0),
+            ]
+        },
+        crs="EPSG:4326",
+    )
+    geodataframe = geodataframe.to_crs(epsg=2193)  # New Zealand Transverse Mercator
+    dp_geodataframe = IterableWrapper(iterable=[geodataframe])
+
+    # Raster data
+    dataarray_day = rioxarray.open_rasterio(
+        filename="https://github.com/GenericMappingTools/gmtserver-admin/raw/master/cache/earth_day_HD.tif",
+    )
+    dataarray_night = rioxarray.open_rasterio(
+        filename="https://github.com/GenericMappingTools/gmtserver-admin/raw/master/cache/earth_night_HD.tif",
+    )
+    dp_dataarray = IterableWrapper(iterable=[dataarray_day, dataarray_night])
+
+    # Using class constructors
+    dp_rasterio = RasterioRasterizer(source_datapipe=dp_geodataframe, out=dp_dataarray)
+    # Using functional form (recommended)
+    dp_rasterio = dp_geodataframe.rasterize_with_rasterio(out=dp_dataarray)
+
+    # Iterate over DataPipe
+    assert len(dp_rasterio) == 2
+    it = iter(dp_rasterio)
+    _ = next(it)  # 1st iteration
+    dataarray = next(it)  # 2nd iteration
+
+    assert dataarray.shape == (1, 960, 1920)
+    assert dataarray.dims == ("band", "y", "x")
+    assert dataarray.dtype == np.uint8
+    assert dataarray.rio.crs == 4326
+    assert dataarray.rio.transform() == dataarray_night.rio.transform()
+    assert dataarray.sum() == 51200
+    assert dataarray.isin(0).sum() == 1792000
+    assert (~dataarray.isin(test_elements=[0, 1])).sum() == 0
