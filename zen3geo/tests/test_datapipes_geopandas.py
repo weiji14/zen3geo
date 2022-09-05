f65c573125692fffb10f63136c2f508b9fe60710
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-09-05 16:51:25 -0400

    :white_check_mark: Unit tests to clip geoseries and check incorrect len
    Ensure that a geopandas.Geoseries vector can be clipped using xarray.Dataset rasters, and that vector datapipes which are too long (len > 1) raise a NotImplementedError properly.

---
+++ zen3geo/tests/test_datapipes_geopandas.py
@@ -0,0 +1,102 @@
+"""
+Tests for geopandas datapipes.
+"""
+import numpy as np
+import pytest
+import xarray as xr
+from torchdata.datapipes.iter import IterableWrapper
+
+from zen3geo.datapipes import GeoPandasRectangleClipper
+
+gpd = pytest.importorskip("geopandas")
+shapely = pytest.importorskip("shapely")
+
+# %%
+@pytest.fixture(scope="module", name="geodataframe")
+def fixture_geodataframe():
+    """
+    A geopandas.GeoDataFrame containing a collection of shapely.geometry
+    objects to use in the tests.
+    """
+    geometries: list = [
+        shapely.geometry.box(minx=0.0, miny=0.0, maxx=2.0, maxy=2.0),
+        shapely.geometry.box(minx=2.0, miny=2.0, maxx=4.0, maxy=4.0),
+    ]
+    geodataframe = gpd.GeoDataFrame(data={"geometry": geometries})
+    geodataframe = geodataframe.set_crs(crs="OGC:CRS84")
+
+    return geodataframe
+
+
+@pytest.fixture(scope="function", name="dataset")
+def fixture_dataset():
+    """
+    The sample xarray.Dataset to use in the tests.
+    """
+    dataarray = xr.DataArray(
+        data=np.ones(shape=(1, 5, 7)),
+        coords=dict(
+            band=[0],
+            y=np.linspace(start=4.0, stop=0.0, num=5),
+            x=np.linspace(start=-1.0, stop=5, num=7),
+        ),
+        dims=("band", "y", "x"),
+        name="foo",
+    )
+    dataset: xr.Dataset = dataarray.to_dataset()
+    dataset: xr.Dataset = dataset.rio.write_crs(input_crs="OGC:CRS84")
+
+    return dataset
+
+
+# %%
+def test_geopandas_rectangle_clipper_geoseries_dataset(geodataframe, dataset):
+    """
+    Ensure that GeoPandasRectangleClipper works to clip a geopandas.GeoSeries
+    vector with an xarray.Dataset raster and outputs a tuple made up of a
+    spatially subsetted geopandas.GeoSeries and the xarray.Dataset raster mask.
+    """
+    dp_vector = IterableWrapper(iterable=[geodataframe.geometry])
+    dp_raster = IterableWrapper(
+        iterable=[
+            dataset.rio.clip_box(minx=-1, miny=0, maxx=1, maxy=1),
+            dataset.rio.clip_box(minx=3, miny=3, maxx=5, maxy=4),
+        ]
+    )
+
+    # Using class constructors
+    dp_clipped = GeoPandasRectangleClipper(
+        source_datapipe=dp_vector, mask_datapipe=dp_raster
+    )
+    # Using functional form (recommended)
+    dp_clipped = dp_vector.clip_vector_with_rectangle(mask_datapipe=dp_raster)
+
+    assert len(dp_clipped) == 2
+    it = iter(dp_clipped)
+
+    clipped_geoseries, raster_chip = next(it)
+    assert clipped_geoseries.crs == "OGC:CRS84"
+    assert all(clipped_geoseries.geom_type == "Polygon")
+    assert clipped_geoseries.shape == (1,)
+    assert clipped_geoseries[0].bounds == (0.0, 0.0, 1.5, 1.5)
+    assert raster_chip.dims == {"band": 1, "y": 2, "x": 3}
+    assert raster_chip.rio.bounds() == (-1.5, -0.5, 1.5, 1.5)
+
+    clipped_geoseries, raster_chip = next(it)
+    assert clipped_geoseries.shape == (1,)
+    assert clipped_geoseries[1].bounds == (2.5, 2.5, 4.0, 4.0)
+    assert raster_chip.dims == {"band": 1, "y": 2, "x": 3}
+    assert raster_chip.rio.bounds() == (2.5, 2.5, 5.5, 4.5)
+    assert raster_chip.rio.crs == "OGC:CRS84"
+
+
+def test_geopandas_rectangle_clipper_incorrect_length(geodataframe, dataset):
+    """
+    Ensure that GeoPandasRectangleClipper raises a NotImplementedError when the
+    length of the vector datapipe is not equal to 1.
+    """
+    dp_vector = IterableWrapper(iterable=[geodataframe, geodataframe])
+    dp_raster = IterableWrapper(iterable=[dataset, dataset, dataset])
+
+    with pytest.raises(NotImplementedError, match="The vector datapipe's length can"):
+        dp_clipped = dp_vector.clip_vector_with_rectangle(mask_datapipe=dp_raster)
